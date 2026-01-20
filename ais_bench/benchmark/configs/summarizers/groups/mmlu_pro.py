5945fe03f9eae19d2ab0ba84d2091ac9160d0143
GaoHuaZhang <1484391106@qq.com>
2026-01-20 09:59:12 +0800

    add mmlu pro weight

--- ais_bench/benchmark/configs/summarizers/groups/mmlu_pro.py
+++ ais_bench/benchmark/configs/summarizers/groups/mmlu_pro.py
@@ -3,3 +3,22 @@ categories = ['math', 'physics', 'chemistry', 'law', 'engineering', 'other', 'ec
 mmlu_pro_summary_groups = [
     {'name': 'mmlu_pro', 'subsets': ['mmlu_pro_' + c.replace(' ', '_') for c in categories]},
 ]
+
+_mmlu_pro_all = ['mmlu_pro_' + c.replace(' ', '_') for c in categories]
+_mmlu_pro_weights = {
+    'mmlu_pro_math': 1351,
+    'mmlu_pro_physics': 1299,
+    'mmlu_pro_chemistry': 1132,
+    'mmlu_pro_law': 1101,
+    'mmlu_pro_engineering': 969,
+    'mmlu_pro_other': 924,
+    'mmlu_pro_economics': 844,
+    'mmlu_pro_health': 818,
+    'mmlu_pro_psychology': 798,
+    'mmlu_pro_business': 789,
+    'mmlu_pro_biology': 717,
+    'mmlu_pro_philosophy': 499,
+    'mmlu_pro_computer_science': 410,
+    'mmlu_pro_history': 381,
+}
+mmlu_pro_summary_groups.append({'name': 'mmlu_pro-weighted', 'subsets': _mmlu_pro_all, 'weights': _mmlu_pro_weights})
